8ce09a797c0db71d004e32e442c0cb550d0ac6e4
shahaf.drori <shahaf.drori@gmail.com>
2025-12-01 12:07:41 +0200

    Skip starter login test (not used)

--- tests/specs/todo.spec.ts
+++ tests/specs/todo.spec.ts
@@ -2,7 +2,7 @@
 import { test } from '../fixtures/test-fixtures';
 
 test.describe('Todo app - happy path', () => {
-  test('user can log in and see the main page', async ({ todoPage }) => {
+  test.skip('user can log in and see the main page', async ({ todoPage }) => {
     await todoPage.goto();
     await todoPage.login();
     await todoPage.expectOnDashboard();
